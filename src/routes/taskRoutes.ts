f4cd07a646ef14fc843ed3756fe3c013773ffcb3
fnder7 <matt.nolimiteron@gmail.com>
2024-06-10 15:15:05 +0300

    fix: update should have been patch method

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.post('/', createTask);
 router.get('/', getTasks);
 router.get('/:id', getTaskById);
-router.put('/:id', updateTask);
+router.patch('/:id', updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;
