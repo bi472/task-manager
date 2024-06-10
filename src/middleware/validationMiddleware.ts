2b20efc09f9e07f9b7574697b3ad725c3725ffe4
fnder7 <matt.nolimiteron@gmail.com>
2024-06-10 15:14:36 +0300

    feat: validation middleware upgraded

--- src/middleware/validationMiddleware.ts
+++ src/middleware/validationMiddleware.ts
@@ -2,16 +2,23 @@ import { plainToInstance } from 'class-transformer';
 import { validate, ValidationError } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
 
+const formatErrors = (errors: ValidationError[]) => {
+    return errors.map(error => {
+        return {
+            property: error.property,
+            constraints: Object.values(error.constraints || {})
+        };
+    });
+};
+
 export const validationMiddleware = (type: any): any => {
     return (req: Request, res: Response, next: NextFunction) => {
         const dto = plainToInstance(type, req.body);
 
         validate(dto).then((errors: ValidationError[]) => {
             if (errors.length > 0) {
-                const messages = errors.map((error: ValidationError) =>
-                    Object.values(error.constraints || {}).join(', ')
-                ).join(', ');
-                res.status(400).json({ error: messages });
+                const formattedErrors = formatErrors(errors);
+                res.status(400).json({ errors: formattedErrors });
             } else {
                 next();
             }
